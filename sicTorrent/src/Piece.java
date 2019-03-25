bdf83a96d09ba5b64a3f227080d5969cc04f0244
abbrima <44316103+abbrima@users.noreply.github.com>
2019-03-25 22:52:39 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	sicTorrent/src/Piece.java

--- sicTorrent/src/Piece.java
+++ sicTorrent/src/Piece.java
@@ -1,9 +1,12 @@
 
 public class Piece {
       private int nBlocks;
-      private int k;
+      private int lastBlockSize;
       private int size;
       public Piece(int size){
           this.size=size;
+
+          nBlocks=size/16384;
+          //int extra=
       }
 }
